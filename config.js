88e99cfdef0735aaf43d85bc532b9ee59d2e0adf
isabellavfortunato <isavfortunato@gmail.com>
2026-06-23 18:25:41 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Cole aqui as chaves do seu projeto Firebase.
+// Enquanto estiver assim, o site salva só neste aparelho.
+// Depois de colar, ele passa a sincronizar entre celular e computador.
+export const firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
